8b3122756d6136ce6fbb89b611606a89b808a0ce
sairam220 <gudidasairam123@gmail.com>
2023-10-05 15:53:11 +0530

    updates

--- src/Components/Seller_customer/Seller_customer.js
+++ src/Components/Seller_customer/Seller_customer.js
@@ -1,13 +1,14 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import * as XLSX from 'xlsx';
 import './index.css';
-
+const error_list=0
 //Creating SellerCustomer Component
 const SellerCustomer=()=> {
 
   //Implementing The State For The Functionality
   const [excelFile,setExcelFile]=useState(null)
   const [typeError,setTypeError]=useState(null)
+  const [erroeList,setErroList]=useState([])
   const [excelData,setExcelData]=useState(null)
   const [invalidData,setInvalidData]=useState(null)
   const [form_name,setFormName]=useState('')
@@ -36,6 +37,11 @@ const SellerCustomer=()=> {
       console.log('Please select your file');
     }
   }
+
+  useEffect(()=>{
+    console.log(invalidData)
+  },[invalidData])
+
 
   // Styles For Table
   const tableStyle = {
@@ -74,9 +80,9 @@ const SellerCustomer=()=> {
   //   <div className="flex justify-center items-center ">
   //     <Rings color="#00BFFF" height={80} width={80} />
   //   </div>
-  // );//
+  // );
 
-  //
+  
 
   function areGSTINandPANSimilar(gstin, pan) {
     // checking Whether PAN AND GSTIN are similar or Not
@@ -104,6 +110,7 @@ const SellerCustomer=()=> {
         if (!row['ERP CUSTOMER CODE']) {
          
           row['Reason'] = "Invalid ERP Code";
+          error_list.push('Invalid ERP Code')
           
         }
         return row;
@@ -113,6 +120,7 @@ const SellerCustomer=()=> {
         if (!row['Legal Name*']) {
          
           row['Reason'] = "Invalid Legal Name";
+          error_list.push('Invalid Legal Name')
           
         }
         return row;
@@ -122,6 +130,7 @@ const SellerCustomer=()=> {
         if (!row['GSTIN']) {
           
           row['Reason'] = "Invalid GSTIN Number";
+          error_list.push('Invalid GSTIN Number')
           
         }
         return row;
@@ -131,7 +140,7 @@ const SellerCustomer=()=> {
         if (!row['PAN']) {
          
           row['Reason'] = "Invalid PAN Number";
-          
+          error_list.push('Invalid PAN Number')    
         }
         return row;
       });
@@ -139,7 +148,7 @@ const SellerCustomer=()=> {
       const mappedInvalidGSTINPAN =allRowsPANANDGSTINNOTMATCHED.map((row) => {
           
           row['Reason'] = "GSTIN & PAN IS NOT Matching";
-          
+          error_list.push('GSTIN & PAN IS NOT Matching')
         
         return row;
       });
@@ -225,6 +234,8 @@ const SellerCustomer=()=> {
         if (!row['Invoice No*']) {
          
           row['Reason'] = "Invalid Invoice Number";
+          error_list.push('Invalid Invoice Number')
+        
           
         }
         return row;
@@ -234,6 +245,7 @@ const SellerCustomer=()=> {
         if (!row['Invoice Date*']) {
          
           row['Reason'] = "Invalid Invoice Date";
+          error_list.push('Invalid Invoice Date')
           
         }
         return row;
@@ -301,6 +313,7 @@ const SellerCustomer=()=> {
       const mappedInvalidCustomerCode =allRowsDOntHaveCustomerCode&& allRowsDOntHaveCustomerCode.map((row) => {
         if (!row['Customer Code']) {
           row['Reason'] = "Invalid Customer Code";
+          error_list.push('Invalid Customer Code')
         }
         return row;
       });
@@ -308,6 +321,7 @@ const SellerCustomer=()=> {
       const mappedInvalidAmountRecived =allRowsDOntHaveAmountRecived&& allRowsDOntHaveAmountRecived.map((row) => {
         if (!row['Amount Received*']) {
           row['Reason'] = "Invalid Amount Received";
+          error_list.push('Invalid Amount Received')
         }
         return row;
       });
@@ -315,6 +329,7 @@ const SellerCustomer=()=> {
       const mappedInvalidCollectedDate =allRowsDOntHaveCollectionDate&&allRowsDOntHaveCollectionDate.map((row) => {
         if (!row['Collection Date*']) {
           row['Reason'] = "Invalid Collection Date";
+          error_list.push('Invalid Collection Date')
         }
         return row;
       });
@@ -334,18 +349,18 @@ const SellerCustomer=()=> {
     setInvalidData(res);}
   }
 
-   //Checking The All The Feilds Whther Valid Or Not For Seller_Payment_Advice File
-   const handleValidationsForSeller_Collections=(data1)=>{
+   //Checking The All The Feilds Whther Valid Or Not For Seller_Collection File
+  const handleValidationsForSeller_Collections=(data1)=>{
     const data=data1.slice(0,20)
     const allRowsHaveValidData = data.filter((row) => 
-    row['ERP Customer Code'] && row['Collection Date*'] && row['Amount Received*'] && row['Customer Code'] && row['Customer Legal Name']  && row['Invoice Date']);
+    row['ERP Customer Code'] && row['Collection Date*'] && row['Amount Received'] && row['Customer Code'] && row['Customer Legal Name']);
     setExcelData(allRowsHaveValidData)
     
     const allRowsDOntHaveERPCode1 = data.filter((row)=>!row['ERP Customer Code'])
     const allRowsDOntHaveLegalName = data.filter((row)=>!row['Customer Legal Name'])
     const allRowsDOntHaveInvoiceNumber = data.filter((row)=>!row['Invoice No'])
     const allRowsDOntHaveCustomerCode = data.filter((row)=>!row['Customer Code'])
-    const allRowsDOntHaveAmountRecived = data.filter((row)=>!row['Amount Received*'])
+    const allRowsDOntHaveAmountRecived = data.filter((row)=>!row['Amount Received'])
     const allRowsDOntHaveCollectionDate = data.filter((row)=>!row['Collection Date*'])
     
     if (allRowsDOntHaveERPCode1.length>0 || allRowsDOntHaveCustomerCode.length>0 || allRowsDOntHaveLegalName.length>0  || allRowsDOntHaveInvoiceNumber.length>0 || allRowsDOntHaveCustomerCode.lenght>0 || allRowsDOntHaveAmountRecived.length>0 || allRowsDOntHaveCollectionDate.length>0){
@@ -379,7 +394,7 @@ const SellerCustomer=()=> {
       });
 
       const mappedInvalidAmountRecived =allRowsDOntHaveAmountRecived&& allRowsDOntHaveAmountRecived.map((row) => {
-        if (!row['Amount Received*']) {
+        if (!row['Amount Received']) {
           row['Reason'] = "Invalid Amount Received";
         }
         return row;
@@ -406,6 +421,244 @@ const SellerCustomer=()=> {
     setInvalidData(res);}
   }
 
+  //Checking The All The Feilds Whther Valid Or Not For Seller_Credit_Note File
+  const handleValidationsForSeller_Crdit_Note=(data1)=>{
+    const data=data1.slice(0,20)
+    const allRowsHaveValidData = data.filter((row) => 
+    row['ERP Customer Code'] && row['Credit Note No*'] && row['Credit Note Date*']&& row['Original Invoice Date*'] && row['GSTIN']  && row['Legal Name'] && row['Original Invoice No*']
+    && row['PAN'] && row['Einvoice IRN'] && row['GSTIN'] && areGSTINandPANSimilar(row['GSTIN'], row['PAN']));setExcelData(allRowsHaveValidData)
+    
+    const allRowsDOntHaveERPCode1 = data.filter((row)=>!row['ERP Customer Code'])
+    const allRowsDOntHaveLegalName = data.filter((row)=>!row['Legal Name'])
+    const allRowsDOntHaveInvoiceNumber = data.filter((row)=>!row['Original Invoice No*'])
+    const allRowsDOntHaveOriginalDate = data.filter((row)=>!row['Original Invoice Date*'])
+    const allRowsDOntHavePAN = data.filter((row)=>!row['PAN'])
+    const allRowsDOntHaveEinvoice = data.filter((row)=>!row['Einvoice IRN'])
+    const allRowsDOntHaveCreditNote = data.filter((row)=>!row['Credit Note No*'])
+    const allRowsDOntHaveCreditDate = data.filter((row)=>!row['Credit Note Date*'])
+    const allRowsDOntHaveGSTIN = data.filter((row)=>!row['GSTIN'])
+    const allRowsPANANDGSTINNOTMATCHED = data.filter((row)=>((row['GSTIN']&& row['PAN'])&&!areGSTINandPANSimilar(row['GSTIN'], row['PAN'])))
+    
+    
+    if (allRowsDOntHavePAN.length>0 || allRowsDOntHaveCreditDate.lenght>0 || allRowsDOntHaveCreditNote.lenght>0 || allRowsDOntHaveEinvoice.lenght>0 || allRowsDOntHaveGSTIN.lenght>0 || allRowsPANANDGSTINNOTMATCHED.length>0 || allRowsDOntHaveERPCode1.length>0 || allRowsDOntHaveLegalName.length>0  || allRowsDOntHaveInvoiceNumber.length>0 ||  allRowsDOntHaveOriginalDate.length>0){
+      
+      const mappedInvalidERPCode =allRowsDOntHaveERPCode1&& allRowsDOntHaveERPCode1.map((row) => {
+        if (!row['ERP Customer Code']) {
+          row['Reason'] = "Invalid ERP Code";
+        }
+        return row;
+      });
+
+      const mappedInvalidCreditNote =allRowsDOntHaveCreditNote&& allRowsDOntHaveCreditNote.map((row) => {
+        if (!row['Credit Note No*']) {
+          row['Reason'] = "Invalid Credit Note No";
+          error_list.push('Invalid Credit Note No')
+        }
+        return row;
+      });
+
+      const mappedInvalidCreditDate =allRowsDOntHaveCreditDate&& allRowsDOntHaveCreditDate.map((row) => {
+        if (!row['Credit Note Date*']) {
+          row['Reason'] = "Invalid Credit Note Date";
+          error_list.push('Invalid Credit Note Date')
+        }
+        return row;
+      });
+
+      const mappedInvalidEinvoice =allRowsDOntHaveEinvoice&& allRowsDOntHaveEinvoice.map((row) => {
+        if (!row['Einvoice IRN']) {
+          row['Reason'] = "Invalid Einvoice IRN";
+          error_list.push('Invalid Einvoice IRNo')
+        }
+        return row;
+      });
+
+      const mappedInvalidLegalName =allRowsDOntHaveLegalName&& allRowsDOntHaveLegalName.map((row) => {
+        if (!row['Legal Name']) {  
+          row['Reason'] = "Invalid Legal Name";  
+        }
+        return row;
+      });
+
+      const mappedInvalidInvoiceNumber =allRowsDOntHaveInvoiceNumber&&allRowsDOntHaveInvoiceNumber.map((row) => {
+        if (!row['Original Invoice No*']) {
+          row['Reason'] = "Invalid Original Invoice Number";
+          error_list.push('Invalid Original Invoice Number')
+        }
+        return row;
+      });
+      
+      const mappedInvalidInvioceDate =allRowsDOntHaveOriginalDate&&allRowsDOntHaveOriginalDate.map((row) => {
+        if (!row['Original Invoice Date*']) {
+          row['Reason'] = "Invalid Original Invoice Date";
+          error_list.push('Invalid Original Invoice Date')
+          
+        }
+        return row;
+      });
+
+      const mappedInvalidPAN =allRowsDOntHavePAN&& allRowsDOntHavePAN.map((row) => {
+        if (!row['PAN']) {
+         
+          row['Reason'] = "Invalid PAN Number";
+          
+        }
+        return row;
+      });
+
+      const mappedInvalidGSTIN =allRowsDOntHaveGSTIN&& allRowsDOntHaveGSTIN.map((row) => {
+        if (!row['GSTIN']) {
+         
+          row['Reason'] = "Invalid GSTIN Number";
+          
+        }
+        return row;
+      });
+
+      const mappedInvalidGSTINPAN =allRowsPANANDGSTINNOTMATCHED.map((row) => {
+          
+          row['Reason'] = "GSTIN & PAN IS NOT Matching";
+          
+        
+        return row;
+      });
+
+      // concating all invalid data by using spred operator
+      const res = [
+        ...mappedInvalidERPCode,
+        ...mappedInvalidLegalName,
+        ...mappedInvalidInvoiceNumber,
+        ...mappedInvalidInvioceDate,
+        ...mappedInvalidPAN,
+        ...mappedInvalidGSTINPAN,
+        ...mappedInvalidGSTIN,
+        ...mappedInvalidEinvoice,
+        ...mappedInvalidCreditDate,
+        ...mappedInvalidCreditNote
+      ];
+
+    setInvalidData(res);}
+  }
+
+  //Checking The All The Feilds Whther Valid Or Not For Seller_Debit_Note File
+  const handleValidationsForSeller_Debit_Note=(data1)=>{
+    
+    const data=data1.slice(0,20)
+    const allRowsHaveValidData = data.filter((row) => 
+    row['ERP Customer Code'] && row['Debit Note No*'] && row['Original Invoice No*']&& row['Debit Note Date*'] && row['Original Invoice Date*'] && row['GSTIN']  && row['Legal Name']
+    && row['PAN'] && row['Einvoice IRN'] && row['GSTIN'] && areGSTINandPANSimilar(row['GSTIN'], row['PAN']));
+    setExcelData(allRowsHaveValidData)
+    
+    const allRowsDOntHaveERPCode1 = data.filter((row)=>!row['ERP Customer Code'])
+    const allRowsDOntHaveLegalName = data.filter((row)=>!row['Legal Name'])
+    const allRowsDOntHaveInvoiceNumber = data.filter((row)=>!row['Original Invoice No*'])
+    const allRowsDOntHaveOriginalDate = data.filter((row)=>!row['Original Invoice Date*'])
+    const allRowsDOntHavePAN = data.filter((row)=>!row['PAN'])
+    const allRowsDOntHaveEinvoice = data.filter((row)=>!row['Einvoice IRN'])
+    const allRowsDOntHaveDebitNote = data.filter((row)=>!row['Debit Note No*'])
+    const allRowsDOntHaveDebitDate = data.filter((row)=>!row['Debit Note Date*'])
+    const allRowsDOntHaveGSTIN = data.filter((row)=>!row['GSTIN'])
+    const allRowsPANANDGSTINNOTMATCHED = data.filter((row)=>((row['GSTIN']&& row['PAN'])&&!areGSTINandPANSimilar(row['GSTIN'], row['PAN'])))
+    
+    
+    if (allRowsDOntHavePAN.length>0 || allRowsDOntHaveDebitDate.lenght>0 || allRowsDOntHaveDebitNote.lenght>0 || allRowsDOntHaveEinvoice.lenght>0 || allRowsDOntHaveGSTIN.lenght>0 || allRowsPANANDGSTINNOTMATCHED.length>0 || allRowsDOntHaveERPCode1.length>0 || allRowsDOntHaveLegalName.length>0  || allRowsDOntHaveInvoiceNumber.length>0 ||  allRowsDOntHaveOriginalDate.length>0){
+      
+      const mappedInvalidERPCode =allRowsDOntHaveERPCode1&& allRowsDOntHaveERPCode1.map((row) => {
+        if (!row['ERP Customer Code']) {
+          row['Reason'] = "Invalid ERP Code";
+        }
+        return row;
+      });
+
+      const mappedInvalidDebitNote =allRowsDOntHaveDebitNote&& allRowsDOntHaveDebitNote.map((row) => {
+        if (!row['Debit Note No*']) {
+          row['Reason'] = "Invalid Debit Note No";
+          error_list.push('Invalid Debit Note No')
+        }
+        return row;
+      });
+
+      const mappedInvalidDebitDate =allRowsDOntHaveDebitDate&& allRowsDOntHaveDebitDate.map((row) => {
+        if (!row['Debit Note Date*']) {
+          row['Reason'] = "Invalid Debit Note Date";
+          error_list.push('Invalid Debit Note Date')
+        }
+        return row;
+      });
+
+      const mappedInvalidEinvoice =allRowsDOntHaveEinvoice&& allRowsDOntHaveEinvoice.map((row) => {
+        if (!row['Einvoice IRN']) {
+          row['Reason'] = "Invalid Einvoice IRN";
+        }
+        return row;
+      });
+
+      const mappedInvalidLegalName =allRowsDOntHaveLegalName&& allRowsDOntHaveLegalName.map((row) => {
+        if (!row['Legal Name']) {  
+          row['Reason'] = "Invalid Legal Name";  
+        }
+        return row;
+      });
+
+      const mappedInvalidInvoiceNumber =allRowsDOntHaveInvoiceNumber&&allRowsDOntHaveInvoiceNumber.map((row) => {
+        if (!row['Original Invoice No*']) {
+          row['Reason'] = "Invalid Original Invoice Number";
+        }
+        return row;
+      });
+      
+      const mappedInvalidInvioceDate =allRowsDOntHaveOriginalDate&&allRowsDOntHaveOriginalDate.map((row) => {
+        if (!row['Original Invoice Date*']) {
+          row['Reason'] = "Invalid Original Invoice Date";
+        }
+        return row;
+      });
+
+      const mappedInvalidPAN =allRowsDOntHavePAN&& allRowsDOntHavePAN.map((row) => {
+        if (!row['PAN']) {
+         
+          row['Reason'] = "Invalid PAN Number";
+          
+        }
+        return row;
+      });
+
+      const mappedInvalidGSTIN =allRowsDOntHaveGSTIN&& allRowsDOntHaveGSTIN.map((row) => {
+        if (!row['GSTIN']) {
+         
+          row['Reason'] = "Invalid GSTIN Number";
+          
+        }
+        return row;
+      });
+
+      const mappedInvalidGSTINPAN =allRowsPANANDGSTINNOTMATCHED.map((row) => {
+          
+          row['Reason'] = "GSTIN & PAN IS NOT Matching";
+          
+        
+        return row;
+      });
+
+      // concating all invalid data by using spred operator
+      const res = [
+        ...mappedInvalidERPCode,
+        ...mappedInvalidLegalName,
+        ...mappedInvalidInvoiceNumber,
+        ...mappedInvalidInvioceDate,
+        ...mappedInvalidPAN,
+        ...mappedInvalidGSTINPAN,
+        ...mappedInvalidGSTIN,
+        ...mappedInvalidEinvoice,
+        ...mappedInvalidDebitDate,
+        ...mappedInvalidDebitNote
+      ];
+
+    setInvalidData(res);}
+  }
+
+
+
   //handling Sumbit Of Excel File
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -429,10 +682,17 @@ const SellerCustomer=()=> {
         case 'Seller_collection.xlsx':
           handleValidationsForSeller_Collections(data)
           break
+        case 'Seller_credit_note.xlsx':
+          handleValidationsForSeller_Crdit_Note(data)
+          break
+        case 'Seller_debit_note.xlsx':
+          handleValidationsForSeller_Debit_Note(data)
+          break
         default:
           break;
       }
     }
+   
   };
  
   //rendering Valid And Invalid Tables for all files based on file Name
@@ -483,15 +743,19 @@ const SellerCustomer=()=> {
                </tr>
              </thead>
              <tbody>
-               {invalidData.map((individualData, index) => (
-                 <tr key={index}>
-                   {Object.keys(individualData).map((key) => (
-                     <td style={tdStyle} key={key} className={(individualData[key] === 'Invalid ERP Code' || individualData[key] === 'Invalid Legal Name' || individualData[key] === 'Invalid GSTIN Number' || individualData[key] === 'Invalid PAN Number' || individualData[key]==='PAN & GSTIN Didnt Matched' || individualData[key]==='GSTIN & PAN IS NOT Matching' || individualData[key]==='Invalid Invoice Number' || individualData[key]==='Invalid Invoice Date') &&'err-msg'}>
-                       {individualData[key]}
-                     </td>
-                   ))}
-                 </tr>
-               ))}
+        {invalidData.map((individualData, index) => (
+<tr key={index}>
+{Object.keys(individualData).map((key, innerIndex) => (
+<td
+style={tdStyle}
+key={key}
+className={key === "Reason" ? "err-msg" : ""}
+>
+{individualData[key]}
+</td>
+))}
+</tr>
+       ))}
              </tbody>
            </table>
          </div>
@@ -534,5 +798,5 @@ const SellerCustomer=()=> {
 
 }
 
-// Exporting App Component
+// Exporting SellerCustomer Component
 export default SellerCustomer;
